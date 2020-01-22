383535320cea50c8c89a0646811caa9d11b59782
arcum-omni <travis.l.eiler@gmail.com>
2020-01-22 14:42:40 -0800

    Created query

---
+++ SMSEntityFramework/StudentDb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSEntityFramework
+{
+    public static class StudentDb
+    {
+        /// <summary>
+        /// Returns a list of all the students
+        /// sorted by StudendId in ascending order
+        /// </summary>
+        /// <returns></returns>
+        public static List<Student> GetAllStudents()
+        {
+            //var context = new StudentContext(); //optional when type is explicit
+            StudentContext context = new StudentContext();
+
+            // LINQ - Language INtegrated Query
+            // LINQ Query Syntax
+            List<Student> students =
+                (from s in context.Students
+                 orderby s.StudentId ascending
+                 select s).ToList();
+            return students;
+        }
+    }
+}
