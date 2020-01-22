a065ba613ca391296d6347fc9710c466d762c2c6
arcum-omni <travis.l.eiler@gmail.com>
2020-01-22 13:52:24 -0800

    Initial Commit

---
+++ SMSEntityFramework/StudentContext.cs
@@ -0,0 +1,33 @@
+namespace SMSEntityFramework
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class StudentContext : DbContext
+    {
+        // Your context has been configured to use a 'StudentContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'SMSEntityFramework.StudentContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'StudentContext' 
+        // connection string in the application configuration file.
+        public StudentContext()
+            : base("name=StudentContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
